962176850b1f216205b8113f7ed5d5d935cd7712
Soroush Beigi <sbeygi712@gmail.com>
2023-12-21 13:36:58 +0330

    Implementing login bloc

--- lib/features/feature_login/presentation/login_screen.dart
+++ lib/features/feature_login/presentation/login_screen.dart
@@ -8,10 +8,36 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  late TextEditingController _emailController ;
+  late TextEditingController _passwordController;
+
+  @override
+  void initState() {
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Center(
+          child: Column(
+        children: [
+          TextField(controller: _emailController,),
+          TextField(controller: _passwordController,),
+          ElevatedButton(onPressed: (){
+            
+          }, child: const Text('login'))
+        ],
+      )),
     );
   }
 }

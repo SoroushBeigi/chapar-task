c37b40a778861939b6a7c3c9e34f6c5a3fecc9aa
Soroush Beigi <sbeygi712@gmail.com>
2023-12-22 18:11:43 +0330

    Making the app portrait-only

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:chapar_task/core/routes/routes.dart';
 import 'package:chapar_task/core/utils/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
 class App extends StatelessWidget {
@@ -8,6 +9,10 @@ class App extends StatelessWidget {
   const App({required this.loggedIn, super.key});
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return ResponsiveSizer(
       builder: (context, orientation, screenType) => MaterialApp.router(
         debugShowCheckedModeBanner: false,
